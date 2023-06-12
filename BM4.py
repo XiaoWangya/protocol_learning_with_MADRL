9a24ce34606a80863e4bf8f2d761249939909961
root <wangzx_wh@126.com>
2023-06-12 15:02:18 +0800

    fix error

--- BM4.py
+++ BM4.py
@@ -9,7 +9,7 @@ from TD3 import TD3  # Importing a class named 'HetGAT_MADRL_PL' located within 
 from utils import *                     # Importing all content from a module 'utils' located within the same directory
 from get_args import args                # Importing argument values from a module 'get_args' located within the same directory
 
-all_random(3417)
+all_random(args.seeds)  
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 parallel_available, _ = gpu_setting()   # Assigning two variables 'parallel_available' and 'num_devices' with the values returned by the function 'gpu_setting'. This function is not defined in this code snippet. It might be defined in one of the imported modules.
 
@@ -17,7 +17,7 @@ device = 'cuda:0' if torch.cuda.is_available() else 'cpu'   # Checking if GPU is
 
 # Setting environment parameters
 n_client = args.n_client                  # Number of clients
-n_subcarrier = max(1, np.floor(args.SCR*n_client))   # Maximum number of subcarriers, as calculated based on user input 'SCR'
+n_subcarrier = min(args.K, n_client)   # Maximum number of subcarriers, as calculated based on user input 'SCR'
 
 # Setting RL (Reinforcement Learning) parameters
 d_input_ue = (args.record_length+1)*2+5   # Input dimensionality of the UE (User Equipment). It is based on 'record_length' and energy records. 
@@ -37,6 +37,10 @@ def main():
         environment = plenv(n_client, n_subcarrier, record_length=args.record_length, channel_model= 'Rayleigh')  # Creating an instance of plenv class with given inputs as parameters.
         grants_his = [0 for _ in range(n_client)]
         access_his = [0 for _ in range(n_client)]
+        request_eng = [0 for _ in range(n_client)]
+        transmit_eng = [0 for _ in range(n_client)]
+        eff_request_eng = [0 for _ in range(n_client)]
+        eff_transmit_eng = [0 for _ in range(n_client)]
         while not environment.done:   # While the environment has not been completed (i.e., done=False)
             epoch += 1    # Incrementing the value of epoch by 1
 
@@ -55,7 +59,7 @@ def main():
             action_bs_r = torch.cat((torch.tensor(round_robin).to(device).unsqueeze(0), action_bs_r), 0)  # Getting predicted action for BS from actor network, which takes current BS state as input
 
             grants = torch.zeros_like(requests)  # Computing 'grants' based on predicted action by binarizing one of the actions and transposing the other.
-            grants[torch.topk(action_bs_r[0, :].masked_fill(requests == 0, float('-inf')), int(args.SCR*args.n_client))[-1]] = requests[torch.topk(action_bs_r[0, :].masked_fill(requests == 0, float('-inf')), int(args.SCR*args.n_client))[-1]]
+            grants[torch.topk(action_bs_r[0, :].masked_fill(requests == 0, float('-inf')), n_subcarrier)[-1]] = requests[torch.topk(action_bs_r[0, :].masked_fill(requests == 0, float('-inf')), n_subcarrier)[-1]]
             power = action_bs_r[1, :]*args.pmax*grants   # Computing the power allocation to each client
             bandwidth = normalized_bandwidth(action_bs_r[2, :], grants)  # Computing the bandwidth allocated to each client after normalizing the input received from actor network.
             RB_blocks = (shrink(power), shrink(bandwidth))   # Shrinking power and bandwidth allocations using 'shrink' function
@@ -76,19 +80,21 @@ def main():
                 # args.action_noise *= (1-5e-4)
             access_his = [requests[_] + access_his[_] for _ in range(n_client)]    
             grants_his = [grants[_] + grants_his[_] for _ in range(n_client)]    
-            
-            Bernard.add_scalars('agent requests', {'ue%d'%index: value for index, value in enumerate(requests)}, global_step=epoch ) 
-            Bernard.add_scalars('BS grants', {'ue%d'%index: value for index, value in enumerate(grants)}, global_step=epoch ) 
-            Bernard.add_scalars('channel', {'ue%d'%index: value for index, value in enumerate(abs(environment.channel))}, global_step=epoch ) 
-        # environment = plenv(n_client, n_subcarrier, record_length=args.record_length)   # Creating a new instance of plenv at the end of each loop.
-        args.action_noise *= (1-5e-3)
+            request_eng = [environment.energy_comsup_list_req[_] + request_eng[_] for _ in range(n_client)]
+            transmit_eng = [environment.energy_comsup_list_tns[_] + transmit_eng[_] for _ in range(n_client)]
+            eff_request_eng = [environment.energy_comsup_list_req[_] + request_eng[_] for _ in range(n_client) if environment.grant_record[_][-1]]
+            eff_transmit_eng = [environment.energy_comsup_list_tns[_] + transmit_eng[_] for _ in range(n_client) if environment.bs_grants_[_]]
+        args.action_noise *= (1-args.decay_factor)
         access_efficiency = [grants_his[_]/max(1, access_his[_]) for _ in range(n_client)]   
         Bernard.add_scalar('rounds exist', environment.rounds, global_step= loops)
         Bernard.add_scalar('Reward', sum(environment.reward_list), global_step=loops)
-        Bernard.add_scalars('energy remain', {'ue%d'%index: value for index, value in enumerate(environment.ue_energy_list)}, global_step=loops )
-        Bernard.add_scalars('Access efficiency', {'ue%d'%index: value for index, value in enumerate(access_efficiency)}, global_step=loops) 
-        Bernard.add_scalar('Average access efficiency', torch.tensor(access_efficiency).mean(), global_step=loops)
-        Bernard.add_scalar('Average energy efficiency', np.log(environment.rounds/(sum(environment.ue_energy_list)+ 1e-50)), global_step=loops)
+        Bernard.add_scalar('Normalized average access efficiency', torch.tensor(access_efficiency).mean()*environment.rounds, global_step=loops)
+        Bernard.add_scalar('Normalized average energy efficiency', (2*n_client - sum(environment.ue_energy_list)) / environment.rounds, global_step=loops)
+        Bernard.add_scalar('Energy consumption in requesting', sum(request_eng), global_step=loops)
+        Bernard.add_scalar('Efficient energy consumption in requesting', sum(eff_request_eng), global_step=loops)
+        Bernard.add_scalar('Energy consumption in transmission', sum(transmit_eng), global_step=loops)
+        Bernard.add_scalar('Efficient energy consumption in transmission', sum(eff_transmit_eng), global_step=loops)
+    Bernard.add_scalar('number of edge devices', n_client)
     Bernard.close()   # Closing the TensorBoard Summary Writer instance
     
 if __name__ == "__main__":
